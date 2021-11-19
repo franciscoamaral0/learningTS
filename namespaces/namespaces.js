2cb791c6e683629d7f2c1f1356cfc49a88c9d561
Franciscoamaral0 <franciscogurgel@me.com>
2021-11-19 09:23:18 +0000

    finish namespaces examples

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,16 +1,15 @@
+"use strict";
 // namespace Geomatria {
 //   export namespace Area {
 //     const PI = 3.14;
-
 //     export function circunferencia(raio: number): number {
 //       return PI * Math.pow(raio, 2);
 //     }
-
 //     export function retangulo(base: number, altura: number): number {
 //       return base * altura;
 //     }
 //   }
 // }
-
 console.log(Geometria.Area.circunferencia(3));
 console.log(Geometria.Area.retangulo(2, 3));
+//# sourceMappingURL=namespaces.js.map
