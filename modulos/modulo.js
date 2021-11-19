dbbd60881cf528e64862584b9fdf014d767e5348
Franciscoamaral0 <franciscogurgel@me.com>
2021-11-19 22:29:46 +0000

    add modules

---
+++ modulos/modulo.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const circunferencia_js_1 = require("./circunferencia.js");
+const retangulo_js_1 = require("./retangulo.js");
+console.log((0, circunferencia_js_1.areaCircunferencia)(3));
+console.log((0, retangulo_js_1.areaRetangulo)(4, 6));
+//# sourceMappingURL=modulo.js.map
---
+++ modulos/modulo.ts
@@ -0,0 +1,6 @@
+import {areaCircunferencia} from './circunferencia.js'
+import { areaRetangulo } from './retangulo.js'
+
+
+console.log(areaCircunferencia(3))
+console.log(areaRetangulo(4,6))
